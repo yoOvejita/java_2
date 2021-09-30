d91553bda4fb44430b4660cd158c27ea76fe1437
rusok <rusok@COMPU-DE-RUB>
2021-09-30 18:39:25 -0400

    Presentación de contenedores/layouts e introducción de controles.

--- miVentana/src/main/java/com/miVentana/FXMLController.java
+++ miVentana/src/main/java/com/miVentana/FXMLController.java
@@ -11,15 +11,39 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class FXMLController implements Initializable {
     
+	@FXML
+	private TextField num1;
+	
+	@FXML
+	private TextField num2;
+	
     @FXML
-    private Label lblOut;
+    private Label resultado;
+    
+    
     
     @FXML
-    private void btnClickAction(ActionEvent event) {
-        lblOut.setText("Hola a todos!");
+    private void dividirNumeros(ActionEvent evento) {
+    	
+    	
+    	try {
+    		int n1 = Integer.parseInt(num1.getText());
+        	int n2 = Integer.parseInt(num2.getText());
+    		int res = n1 / n2;
+    		resultado.setText(res + "");
+    	}catch(ArithmeticException e) {
+    		resultado.setText("No divida entre 0 por favor");
+    	}catch(NumberFormatException e2) {
+    		resultado.setText("No ingrese letras");
+    		num1.setText("");
+    		num2.setText("");
+    	}
+    	//continua normalmente
+    	
     }
     
     @Override
